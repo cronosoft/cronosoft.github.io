b87ae04b510d42ca9d50b4d8942584a5b64080c6
Diana <97.diana@gmail.com>
2015-09-09 17:57:19 +0300

    libs added

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var scrollTo = function(element) {
+	$("html, body").animate({ scrollTop: $(element).offset().top}, 1000);
+}
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if( $(this).attr("href")=="#carousel") return;
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+$(function () {
+    var currentURL = ""
+    $(document).scroll(function () {
+        $('.anchor').each(function () {
+            var top = window.pageYOffset;
+            var distance = top - $(this).offset().top;
+            var URL = $(this).attr('name');
+
+            if (distance < 200 && distance > -200 && currentURL != URL) {
+                if (currentURL != "") $("#" + currentURL + "-menu").removeClass('active');
+                currentURL = URL;
+                $("#" + currentURL + "-menu").addClass('active');
+            }
+        });
+    });
+});
